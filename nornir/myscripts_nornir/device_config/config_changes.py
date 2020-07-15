787952fe396e580a9e7c3f8b1e22f82487e7cc16
swamaki <swamaki@gmail.com>
2020-07-14 22:15:12 -0400

    nornir -- eigrp configs

--- nornir/myscripts_nornir/device_config/config_changes.py
+++ nornir/myscripts_nornir/device_config/config_changes.py
@@ -5,10 +5,10 @@ from nornir.plugins.tasks.networking import netmiko_send_config, netmiko_send_co
 nr = InitNornir(config_file="/Users/stephenamaki/Dropbox/netdevops/nornir/config.yaml", dry_run=True)
 
 def baseconfig(command_output):
-    command_output.run(task=netmiko_send_config, config_file= "base_config.cfg")
-    command_output.run(task=netmiko_send_command, command_string = "show run | in ntp")
+    command_output.run(task=netmiko_send_config, config_file= "config_file.cfg")
+    command_output.run(task=netmiko_send_command, command_string = "show run | in scp")
 
-targets = nr.filter (role="router")
+targets = nr.filter (country="usa")
 results = targets.run(task = baseconfig)
 
 print_title("DEPLOYING CONFIGURATIONS")
